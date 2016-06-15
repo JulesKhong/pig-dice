33d5e25b5e2d9655e9345e44c5ce4116fa53ba18
Brett New and Julia Dickey <brett@>
2016-06-15 11:36:21 -0700

    Build feedback to display rolled numbers and most recent roll in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 // Back-end
-var upper = 6;
-var getRandomNumber = function (){
-  var number = Math.floor(Math.random() * upper)+1;
-  alert(number);
+function Game (score){
+  this.score = score;
+  this.roll = [];
 }
 
+var newGame = new Game;
+var randomNumber;
+var upper = 6;
+var rollValue;
 
-
+var getRandomNumber = function() {
+  // debugger;
+  randomNumber = Math.floor(Math.random() * upper)+1;
+  if (randomNumber === 1){
+  rollValue = 0;
+  newGame.roll.push(rollValue);
+  }
+  else {
+  rollValue = randomNumber;
+  newGame.roll.push(rollValue);
+  }
+}
 
 
 
@@ -15,6 +29,10 @@ $(document).ready(function(){
  $("#roll-player1").click(function(event){
    event.preventDefault();
    getRandomNumber();
+   var last = newGame.roll
+   $("#current-roll1").show();
+   $("#current-roll1").text(newGame.roll[newGame.roll.length - 1]);
+   $("#current-series1").text(newGame.roll);
 
  });
 });
